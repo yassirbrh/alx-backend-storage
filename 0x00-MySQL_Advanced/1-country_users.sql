d19bf37097dbec648f77e682f69f24dc7b6bc0cb
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-18 19:19:01 +0000

    SQL script that creates a table users with addition of the country attribute.

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL Script that creates a table users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
